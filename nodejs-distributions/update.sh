268309688c2879b3548919d843c7375aa89cf3d3
Sannie Kwakman <skwakman@xebia.com>
2014-01-26 15:03:58 +0100

    reverted inclusion of npm to distribution. Upgraded nodeJS binaries to 0.10.25

--- nodejs-distributions/update.sh
+++ nodejs-distributions/update.sh
@@ -30,18 +30,12 @@ downloadNodeJs(){
   cd temp_tar
   curl -# -f -o nodejs.tar.gz $downloadUrl || error_exit "Error while downloading $downloadUrl"
 
-  echo "Extracting and re-tarring.."
+  echo "Extracting node binary from tarfile.."
 
-  # Sadly, all nodejs .tar.gz files include a platform-version specific directory.
-  # So we remove that here by re-tarring the file
-  tar zxf nodejs.tar.gz || error_exit "Error while extracting downloaded nodejs file"
-  rm nodejs.tar.gz || error_exit "Could not clean up downloaded tar.gz file"
-  # remove subdirectory added by .tar.gz file and make a new tar
-  cp -R $filename/ . || error_exit "Error while removing subdirectory created by downloaded file"
-  rm -Rf $filename
-  tar -pczf ../nodejs.tar.gz * || error_exit "Could not recreate tar.gz for $platform"
+  tar zxf nodejs.tar.gz $filename/bin/node || error_exit "Error while extracting node binary from downloaded .tar.gz"
+  cp $filename/bin/node ../node || error_exit "Error while copying node binary"
   cd ..
-  rm -Rf temp_tar
+  rm -Rf temp_tar || error_exit "Error while cleaning up download directory"
 }
 
 ## A non-MSI distribution of node for Windows is only available as a single node.exe file
@@ -55,39 +49,9 @@ downloadAndExtractNodeJsWindows(){
   fi
 
   echo "downloading node.exe $downloadUrl"
-  mkdir bin
-  curl -# -f -o bin/node.exe $downloadUrl || error_exit "Error while downloading $downloadUrl"
+  curl -# -f -o node.exe $downloadUrl || error_exit "Error while downloading $downloadUrl"
 }
 
-# download separate npm distribution and append it to the windows dists and create tar.gz from it
-appendNpmToWindowsDirs() {
-  # first, determine which NPM version is used
-  rm -Rf nodejs_tmp
-  mkdir nodejs_tmp
-  cd nodejs_tmp || error_exit "Error creating/changing to nodejs temporary directory"
-  tar zxf ../linux-x64/nodejs.tar.gz --strip=3 '*/npm/package.json' || error_exit "Error while extracting nodejs to find out which NPM version to download"
-  local npmVersion=`cat package.json | grep -o '"version":.*,' | sed -n 's/.*"version": "\(.*\)",/\1/p'`
-  cd ..
-  rm -Rf nodejs_tmp
-
-  local npmDownloadUrl="$NPM_URL_PREFIX/npm-$npmVersion.zip"
-  echo "downloading NPM $npmVersion from $npmDownloadUrl"
-  curl -# -f -o npm.zip $npmDownloadUrl || error_exit "Error while downloading $downloadUrl" || error_exit "Could not download NPM"
-
-  for windowsDistDir in "$@"
-  do
-    echo "creating node+NPM distributions for $windowsDistDir"
-    unzip -qo npm.zip -d $windowsDistDir/bin || error_exit "Could not extract downloaded NPM zip to $windowsDistDir"
-    # create gzip file
-    cd $windowsDistDir
-    tar -pczf ../nodejs.tar.gz * || error_exit "Could not create tar.gz for $windowsDistDir"
-    cd ..
-    rm -Rf $windowsDistDir/* || error_exit "Could not empty $windowsDistDir"
-    cp nodejs.tar.gz $windowsDistDir || error_exit "Could not copy generated nodejs dist to $windowsDistDir"
-    rm nodejs.tar.gz || error_exit "Could not clean up generated nodejs.tar.gz for $windowsDistDir"
-  done
-  rm npm.zip || error_exit "Could not delete downloaded NPM zip file"
-}
 
 # enter a dist subdir, clear contents and download & extract nodejs into it
 updateDistDirectory() {
@@ -120,7 +84,6 @@ updateDistDirectory "linux-x64" "linux-x64"
 updateDistDirectory "win-x86" "win-x86"
 updateDistDirectory "win-x64" "win-x64"
 
-appendNpmToWindowsDirs "win-x64" "win-x86"
 
 echo "finished!"
 
